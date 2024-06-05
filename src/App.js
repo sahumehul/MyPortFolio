6b55f3e389721f571fe0420dc6600b55794f6ef1
Mehul Sahu <mehulyashams@gmail.com>
2024-06-05 23:41:41 +0530

    ft(hero section added)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Hero from './componants/Hero/Hero';
 import Navbar from './componants/Navbar/Navbar';
 
 function App() {
   return (
     <>
       <Navbar/>
+      <div className='container'>
+        <Hero/>
+      </div>
     </>
   );
 }
---
+++ src/componants/Hero/Hero.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import "./Hero.css"
+
+const Hero = () => {
+    return (
+        <section className='hero-container'>
+            <div className='hero-content'>
+                <h2>
+                    Building Digital Experiences that Inspires
+                </h2>
+                <p>
+                    Passionate MERN Stack Developer || Transforming ideas into Seamless and Visually Stunning Web Solutions
+                </p>
+            </div>
+
+            <div className='hero-img'>
+                <div>
+                    <div className='tech-icon'>
+                        <img src='./assets/images/react.jpg' alt=''></img>
+                    </div>
+                    <img src='./assets/images/profile.jpg' alt=''></img>
+                </div>
+
+                <div className='tech-icon'>
+                    <div>
+                        <img src='./assets/images/html.png' alt=''></img>
+                    </div>
+                    <div>
+                        <img src='./assets/images/css.png' alt=''></img>
+                    </div>
+                    <div>
+                        <img src='./assets/images/javascript.png' alt=''></img>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
